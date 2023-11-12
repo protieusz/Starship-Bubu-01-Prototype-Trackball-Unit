1b06f22c5a79e82776436a180830f7d943ec1ec8
ProtieusKeebs <118025702+protieusz@users.noreply.github.com>
2023-11-11 23:50:11 -0800

    Added notes for keymaps

--- starshipbubu02/keymaps/default/keymap.c
+++ starshipbubu02/keymaps/default/keymap.c
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
     [_BASE] = LAYOUT(
 
-        CPI_SW,             RGB_TOG, 
-        KC_MS_BTN1, SCRL_MO, KC_MS_BTN2
+        CPI_SW/*This is CPI setting, total of 5 setting slow to fast, press to toggle*/,             RGB_TOG, 
+        KC_MS_BTN1, SCRL_MO,/*This is scroll lock, press key & use trackball to scroll*/KC_MS_BTN2
     ) 
 };
 
